5028cc8ef09f09ff2467214408df0edb33b71662
OchirErkhembayar <ochir_erkhembayar@yahoo.com>
2023-12-06 10:46:04 +0000

    Optimisations

--- src/solutions/six.rs
+++ src/solutions/six.rs
@@ -1,18 +1,18 @@
 pub fn part_one(data: &str) -> i32 {
     let now = std::time::Instant::now();
 
-    for _ in 0..20000 {
+    for _ in 0..50000 {
         part_one_inner(data);
     }
 
     let elapsed = now.elapsed();
-    println!("Day 6 part 1: {}", elapsed.as_nanos() / 20000);
+    println!("Day 6 part 1: {}", elapsed.as_nanos() / 50000);
     part_one_inner(data)
 }
 
 fn part_one_inner(data: &str) -> i32 {
     let (times, distances) = data.split_once('\n').unwrap();
-    let mut times = times.split_whitespace();
+    let mut times = times.split_ascii_whitespace();
     times.next().unwrap();
     let times = times.map(|t| t.parse::<i32>().unwrap());
     let mut distances = distances.split_ascii_whitespace();
@@ -37,34 +37,40 @@ fn part_one_inner(data: &str) -> i32 {
 pub fn part_two(data: &str) -> i32 {
     let now = std::time::Instant::now();
 
-    for _ in 0..20000 {
+    for _ in 0..50000 {
         part_two_inner(data);
     }
 
     let elapsed = now.elapsed();
-    println!("Day 6 part 2: {}", elapsed.as_nanos() / 20000);
+    println!("Day 6 part 2: {}", elapsed.as_nanos() / 50000);
     part_two_inner(data) as i32
 }
 
 pub fn part_two_inner(data: &str) -> f64 {
     let (times, distances) = data.split_once('\n').unwrap();
-    let time = times
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<String>()
-        .parse::<i64>()
-        .unwrap();
-    let distance = distances
-        .chars()
-        .filter(|c| c.is_digit(10))
-        .collect::<String>()
-        .parse::<i64>()
-        .unwrap();
+    let time: f64 = times
+        .bytes()
+        .skip(11)
+        .rfold((0, 0), |(i, sum), c| {
+            if c != 32 {
+                (i + 1, sum + (c as i64 - '0' as i64) * 10_i64.pow(i as u32))
+            } else {
+                (i, sum)
+            }
+        })
+        .1 as f64;
+    let distance: i64 = distances
+        .bytes()
+        .skip(11)
+        .rfold((0, 0), |(i, sum), c| {
+            if c != 32 {
+                (i + 1, sum + (c as i64 - '0' as i64) * 10_i64.pow(i as u32))
+            } else {
+                (i, sum)
+            }
+        })
+        .1;
 
-    let max_t =
-        ((time as f64 + f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor();
-    let min_t =
-        ((time as f64 - f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor();
-
-    max_t - min_t
+    let sqrt_p = f64::sqrt(((time as i64).pow(2) - 4 * (distance + 1)) as f64);
+    (((time + sqrt_p) / 2.0) - ((time - sqrt_p) / 2.0)).floor()
 }
