ff5134fcef91c976fac97346fd5227f73ac3b1c6
OchirErkhembayar <ochir_erkhembayar@yahoo.com>
2023-12-06 09:43:13 +0000

    Optimisations for day 6 part 2

--- src/solutions/six.rs
+++ src/solutions/six.rs
@@ -1,10 +1,13 @@
 pub fn part_one(data: &str) -> i32 {
     let now = std::time::Instant::now();
-    let ans = part_one_inner(data);
+
+    for _ in 0..20000 {
+        part_one_inner(data);
+    }
 
     let elapsed = now.elapsed();
-    println!("Day 6 part 1: {}", elapsed.as_micros());
-    ans
+    println!("Day 6 part 1: {}", elapsed.as_nanos() / 20000);
+    part_one_inner(data)
 }
 
 fn part_one_inner(data: &str) -> i32 {
@@ -33,32 +36,35 @@ fn part_one_inner(data: &str) -> i32 {
 
 pub fn part_two(data: &str) -> i32 {
     let now = std::time::Instant::now();
-    let ans = part_two_inner(data);
+
+    for _ in 0..20000 {
+        part_two_inner(data);
+    }
 
     let elapsed = now.elapsed();
-    println!("Day 6 part 2: {}", elapsed.as_micros());
-    ans
+    println!("Day 6 part 2: {}", elapsed.as_nanos() / 20000);
+    part_two_inner(data) as i32
 }
 
-pub fn part_two_inner(data: &str) -> i32 {
+pub fn part_two_inner(data: &str) -> f64 {
     let (times, distances) = data.split_once('\n').unwrap();
-    let time = {
-        let mut times = times.split_whitespace();
-        times.next().unwrap();
-        times.collect::<Vec<_>>().join("").parse::<i64>().unwrap()
-    };
-    let distance = {
-        let mut distances = distances.split_ascii_whitespace();
-        distances.next().unwrap();
-        distances
-            .collect::<Vec<_>>()
-            .join("")
-            .parse::<i64>()
-            .unwrap()
-    };
+    let time = times
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse::<i64>()
+        .unwrap();
+    let distance = distances
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect::<String>()
+        .parse::<i64>()
+        .unwrap();
 
-    let max_t = ((time as f64 + f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor() as i64;
-    let min_t = ((time as f64 - f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor() as i64;
+    let max_t =
+        ((time as f64 + f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor();
+    let min_t =
+        ((time as f64 - f64::sqrt((time.pow(2) - 4 * (distance + 1)) as f64)) / 2.0).floor();
 
-    (max_t - min_t) as i32
+    max_t - min_t
 }
