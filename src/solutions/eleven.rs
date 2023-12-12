f5c9a81d6476c4350ef61e0b8593719e0f50c867
OchirErkhembayar <ochir_erkhembayar@yahoo.com>
2023-12-12 17:28:44 +0000

    d11 done

--- src/solutions/eleven.rs
+++ src/solutions/eleven.rs
@@ -6,38 +6,8 @@ pub fn part_one(data: &str) -> i128 {
     ans as i128
 }
 
-fn part_one_inner(data: &str) -> i32 {
-    let mut universe =
-        data.lines()
-            .enumerate()
-            .fold(Universe::default(), |mut universe, (y, line)| {
-                line.chars().enumerate().for_each(|(x, pnt)| {
-                    let kind = match pnt {
-                        '#' => Kind::Galaxy,
-                        '.' => Kind::Space,
-                        _ => unreachable!(),
-                    };
-                    let pnt = Point {
-                        x: x as i32,
-                        y: y as i32,
-                        kind,
-                    };
-                    universe.add_pnt(pnt);
-                });
-                universe
-            });
-    universe.expand();
-
-    println!(
-        "Galaxies: {:?}",
-        universe
-            .points
-            .iter()
-            .filter(|p| p.kind == Kind::Galaxy)
-            .collect::<Vec<_>>()
-    );
-
-    42
+fn part_one_inner(data: &str) -> i128 {
+    run(data, 1)
 }
 
 pub fn part_two(data: &str) -> i128 {
@@ -48,32 +18,44 @@ pub fn part_two(data: &str) -> i128 {
     ans as i128
 }
 
-fn part_two_inner(_data: &str) -> i32 {
-    42
+fn part_two_inner(data: &str) -> i128 {
+    run(data, 999999)
+}
+
+fn run(data: &str, expand_amount: i32) -> i128 {
+    data.lines()
+        .enumerate()
+        .fold(Universe::default(), |mut universe, (y, line)| {
+            line.chars().enumerate().for_each(|(x, pnt)| {
+                if pnt == '#' {
+                    let pnt = Galaxy {
+                        x: x as i32,
+                        y: y as i32,
+                    };
+                    universe.add_pnt(pnt);
+                }
+            });
+            universe
+        })
+        .expand(expand_amount)
+        .calc_edges()
 }
 
 #[derive(Debug, Clone, Default)]
 struct Universe {
-    points: Vec<Point>,
+    points: Vec<Galaxy>,
     width: i32,  // 0 indexed
     height: i32, // 0 indexed
 }
 
-#[derive(Debug, PartialEq, Clone)]
-enum Kind {
-    Space,
-    Galaxy,
-}
-
 #[derive(Debug, Clone, PartialEq)]
-struct Point {
+struct Galaxy {
     x: i32,
     y: i32,
-    kind: Kind,
 }
 
 impl Universe {
-    fn add_pnt(&mut self, pnt: Point) {
+    fn add_pnt(&mut self, pnt: Galaxy) {
         if pnt.x > self.width {
             self.width = pnt.x;
         }
@@ -83,58 +65,62 @@ impl Universe {
         self.points.push(pnt);
     }
 
-    fn expand(&mut self) {
+    fn calc_edges(&self) -> i128 {
+        self.points
+            .iter()
+            .map(|g| (g.x, g.y))
+            .fold(0, |utotal, (x, y)| {
+                self.points
+                    .iter()
+                    .filter(|p| {
+                        if p.y > y {
+                            true
+                        } else if p.y == y {
+                            p.x > x
+                        } else {
+                            false
+                        }
+                    })
+                    .map(|g| (g.x, g.y))
+                    .fold(utotal, |gtotal, (gx, gy)| {
+                        let dx = x.abs_diff(gx) as i128;
+                        let dy = y.abs_diff(gy) as i128;
+                        dy + dx + gtotal
+                    })
+            })
+    }
+
+    fn expand(&mut self, amount: i32) -> &mut Self {
         let mut index = 0;
         while index <= self.width {
-            let galaxies = self
-                .points
-                .iter()
-                .filter(|p| p.kind == Kind::Galaxy && p.x == index)
-                .count();
+            let galaxies = self.points.iter().filter(|p| p.x == index).count();
             if galaxies == 0 {
-                println!("Cloning col at {index}");
-                // Shift everything right once
-                let mut cloned_points = vec![];
                 self.points.iter_mut().for_each(|p| {
-                    if p.x == index {
-                        cloned_points.push(p.clone());
-                    }
-                    if p.x >= index {
-                        p.x += 1;
+                    if p.x > index {
+                        p.x += amount;
                     }
                 });
-                self.width += 1;
-                self.points.append(&mut cloned_points);
-                index += 2;
+                self.width += amount;
+                index += amount + 1;
             } else {
                 index += 1;
             }
         }
         let mut index = 0;
         while index <= self.height {
-            let galaxies = self
-                .points
-                .iter()
-                .filter(|p| p.kind == Kind::Galaxy && p.y == index)
-                .count();
+            let galaxies = self.points.iter().filter(|p| p.y == index).count();
             if galaxies == 0 {
-                println!("Cloning row at {index}");
-                // Shift everything right once
-                let mut cloned_points = vec![];
                 self.points.iter_mut().for_each(|p| {
-                    if p.y == index {
-                        cloned_points.push(p.clone());
-                    }
-                    if p.y >= index {
-                        p.y += 1;
+                    if p.y > index {
+                        p.y += amount;
                     }
                 });
-                self.height += 1;
-                self.points.append(&mut cloned_points);
-                index += 2;
+                self.height += amount;
+                index += amount + 1;
             } else {
                 index += 1;
             }
         }
+        self
     }
 }
